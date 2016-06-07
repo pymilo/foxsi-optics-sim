6761261dd1be5b78f1d1b9d77d70a60b9d6f2d88
pymilo <jcbuitragoc@unal.edu.co>
2016-06-06 17:46:52 -0700

    README and setup.py
    After checking the installation instructions were correct, I
    conveniently change and include some lines to make clear the
    installation process to the users.

--- setup.py
+++ setup.py
@@ -1,27 +1,30 @@
 '''
 Created on Aug 1, 2011
-
 @author: rtaylor
+
+Updated on June 2016
+@ Steven Christe and Milo Buitrago-Casas
+
+Needs Python 2.7
 '''
 from distutils.core import setup
 from os import system
-#from PyQt4.uic import compileUi
 
 srcDir = 'src'
 guiDir = 'src/foxsisim_gui/'
-scpDir = 'src/scripts/'
-scripts = ['foxsisim-gui.py','example1.py','example2.py','example3.py','example4.py','example5.py']
+scpDir = 'examples/'
+gui = ['foxsisim-gui.py']
+scripts = ['example1.py','example2.py','example3.py','example4.py','example5.py','example6.py','example7.py','example8.py']
 
 command = 'pyuic4 -o ' + guiDir + 'ui_mainwindow.py ' + guiDir + 'ui/mainwindow.ui'
-print command
+#print(command)
 system(command)
-#compileUi(guiDir+'ui/mainwindow.ui',file(guiDir+'ui_mainwindow.py','w'))
 
 setup(name='foxsisim',
       version='0.1',
       description='FOXSI Optics Simulation Tool: A python tool to simulate grazing incidence optics response to light sources of different wavelengths.',
-      author='Robert Taylor',
-      author_email='rlvtaylor@embarqmail.com',
+      author='Robert Taylor, Steven Christe',
+      author_email='rlvtaylor@embarqmail.com, steven.d.christe@nasa.gov',
       url='https://github.com/foxsi/foxsi-optics-sim',
       package_dir={'':srcDir},
       packages=['foxsisim','foxsisim_gui'],
